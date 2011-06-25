94a7022a7e8de7f40aa2a634717f414990ebb145
karottenreibe <k@rottenrei.be>
2011-06-25 20:05:54 +0200

    added more tests for timeframes

--- test/test_dates.rb
+++ test/test_dates.rb
@@ -2,17 +2,57 @@ require 'helper'
 
 class TestDates < Test::Unit::TestCase
 
-  context "a Date" do
+  def setup
+      Prunr::Timeframe.extend_fixnum
+  end
 
+  context "a Date" do
     should "be avilable as a DSL method on Number" do
-      Prunr::Date.extend_fixnum
-      assert 1.year.is_a?(Prunr::Date::Year)
-      assert 1.month.is_a?(Prunr::Date::Month)
-      assert 1.week.is_a?(Prunr::Date::Week)
-      assert 1.day.is_a?(Prunr::Date::Day)
+      assert 1.year.is_a?(Prunr::Timeframe::Year)
+      assert 1.month.is_a?(Prunr::Timeframe::Month)
+      assert 1.week.is_a?(Prunr::Timeframe::Week)
+      assert 1.day.is_a?(Prunr::Timeframe::Day)
       assert 1.day.amount == 1
     end
 
+    should "always contain future dates" do
+      assert 1.day.contains(DateTime.now + 3600)
+      assert 1.week.contains(DateTime.now + 3600)
+      assert 1.month.contains(DateTime.now + 3600)
+      assert 1.year.contains(DateTime.now + 3600)
+    end
+  end
+
+  context "a Day" do
+    should "contain some dates" do
+      assert 1.day.contains(DateTime.now)
+      assert 1.day.contains(DateTime.now - 3600)
+      assert !1.day.contains(DateTime.now - 3600*24*2)
+    end
+  end
+
+  context "a Week" do
+    should "contain some dates" do
+      assert 1.week.contains(DateTime.now)
+      assert 1.week.contains(DateTime.now - 3600)
+      assert !1.week.contains(DateTime.now - 3600*24*8)
+    end
+  end
+
+  context "a Month" do
+    should "contain some dates" do
+      assert 1.month.contains(DateTime.now)
+      assert 1.month.contains(DateTime.now - 3600)
+      assert !1.month.contains(DateTime.now - 3600*24*33)
+    end
+  end
+
+  context "a Year" do
+    should "contain some dates" do
+      assert 1.year.contains(DateTime.now)
+      assert 1.year.contains(DateTime.now - 3600)
+      assert !1.year.contains(DateTime.now - 3600*24*300)
+    end
   end
 
 end
