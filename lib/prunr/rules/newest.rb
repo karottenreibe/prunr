84ca69bbfeaa8fe212d78224491e0f6d8d2114b5
karottenreibe <k@rottenrei.be>
2011-06-29 19:32:00 +0200

    removed flawed filter implementation of :newest

--- lib/prunr/rules/newest.rb
+++ lib/prunr/rules/newest.rb
@@ -10,21 +10,7 @@ class Prunr::Rule::Newest
 
   ## only keep the newest item in every @selection within @timeframe
   def filter(objects)
-    filterable, too_old = objects.find { |o| @timeframe.contains(o.date) }
-    groups = []
-    filterable.each do |o|
-      g = groups.find { |g| @selection.is_between(g[0], o) }
-      if g
-        g << o
-      else
-        groups << [o]
-      end
-    end
-    groups.map do |g|
-      g.sort!
-      g = g[0...@amount]
-    end
-    return too_old + removable
+    objects
   end
 
 end
