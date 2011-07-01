16548ecbd805c1a2fd3e8a80a7d693f277b437b9
karottenreibe <k@rottenrei.be>
2011-07-01 08:47:08 +0200

    fixed Year roster tests

--- lib/prunr/timerosters/year.rb
+++ lib/prunr/timerosters/year.rb
@@ -4,10 +4,14 @@ class Prunr::TimeRoster::Year < Prunr::TimeRoster
 
   def initialize(timeframe)
     super(timeframe, nil)
+    @years = timeframe.days / 365 #todo test this
   end
 
   def adjust_timeframe(timeframe)
-    timeframe
+    date = timeframe.start_date
+    year = date.year
+    diff = year % @years
+    timeframe.start_date = Date.civil(year - diff, 1, 1)
   end
 
 end
